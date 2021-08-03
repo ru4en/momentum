ab8193560eef5f3b1e880c7f067fbb934bada84b
Ruben Lopes <60962448+ru4en@users.noreply.github.com>
2021-08-03 18:13:57 +0000

    Add files via upload

--- momentum/settings.py
+++ momentum/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!%10=h91!pb!l#x0m-@$!r_!kseg1@3tp7(7*kv#mo4fix4hl1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
