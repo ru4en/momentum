e9f211f72a93e6fc2ff519882168759bad49fe8d
Ruben Lopes <60962448+ru4en@users.noreply.github.com>
2021-08-03 16:19:31 +0000

    Update settings.py

--- momentum/settings.py
+++ momentum/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
